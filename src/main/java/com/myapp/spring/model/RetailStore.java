c692b1bec75b420ceace09a385c1aec5d595b0fe
himanirajput <43176597+himanirajput@users.noreply.github.com>
2021-03-16 16:25:05 +0530

    Update RetailStore.java

--- src/main/java/com/myapp/spring/model/RetailStore.java
+++ src/main/java/com/myapp/spring/model/RetailStore.java
@@ -2,108 +2,108 @@ package com.myapp.spring.model;
 
 public class RetailStore {
 
-	private Integer Id;
+	private Integer id;
 
-	private String Category;
+	private String category;
 
 	public Integer getId() {
-		return Id;
+		return id;
 	}
 
 	public void setId(Integer id) {
-		Id = id;
+		this.id = id;
 	}
 
 	public String getCategory() {
-		return Category;
+		return category;
 	}
 
 	public void setCategory(String category) {
-		Category = category;
+		this.category = category;
 	}
 
 	public String getType() {
-		return Type;
+		return type;
 	}
 
 	public void setType(String type) {
-		Type = type;
+		this.type = type;
 	}
 
 	public String getName() {
-		return Name;
+		return name;
 	}
 
 	public void setName(String name) {
-		Name = name;
+		this.name = name;
 	}
 
 	public String getBrand() {
-		return Brand;
+		return brand;
 	}
 
 	public void setBrand(String brand) {
-		Brand = brand;
+		this.brand = brand;
 	}
 
 	public String getQuantity() {
-		return Quantity;
+		return quantity;
 	}
 
 	public void setQuantity(String quantity) {
-		Quantity = quantity;
+		this.quantity = quantity;
 	}
 
 	public Integer getPrice() {
-		return Price;
+		return price;
 	}
 
 	public void setPrice(Integer price) {
-		Price = price;
+		this.price = price;
 	}
 
 	public String getExpiry_Date() {
-		return Expiry_Date;
+		return expiry_Date;
 	}
 
 	public void setExpiry_Date(String expiry_Date) {
-		Expiry_Date = expiry_Date;
+		this.expiry_Date = expiry_Date;
 	}
 
 	public Integer getStock() {
-		return Stock;
+		return stock;
 	}
 
 	public void setStock(Integer stock) {
-		Stock = stock;
+		this.stock = stock;
 	}
 
 	public RetailStore(Integer id, String category, String type, String name, String brand, String quantity,
 			Integer price, String expiry_Date, Integer stock) {
-		Id = id;
-		Category = category;
-		Type = type;
-		Name = name;
-		Brand = brand;
-		Quantity = quantity;
-		Price = price;
-		Expiry_Date = expiry_Date;
-		Stock = stock;
+		this.id = id;
+		this.category = category;
+		this.type = type;
+		this.name = name;
+		this.brand = brand;
+		this.quantity = quantity;
+		this.price = price;
+		this.expiry_Date = expiry_Date;
+		this.stock = stock;
 	}
 
-	private String Type;
+	private String type;
 
-	private String Name;
+	private String name;
 
-	private String Brand;
+	private String brand;
 
-	private String Quantity;
+	private String quantity;
 
-	private Integer Price;
+	private Integer price;
 
-	private String Expiry_Date;
+	private String expiry_Date;
 
-	private Integer Stock;
+	private Integer stock;
 
 	public RetailStore() {
 	}
